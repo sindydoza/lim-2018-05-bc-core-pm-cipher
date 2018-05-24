b220d3590c0d0770e07899650907bc468e3278b6
SinsyDoza <sindyds@gmail.com>
2018-05-24 18:00:53 -0500

    Subiendo Funcion Cifrado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-var abecedario = ("abcdefghijklmnñopqrstuvwxyz");
-var Mensaje;
+//var texto= prompt("ingrese un texo");
+//var clave=parseInt(prompt("ingrese una llave"));
+
+var botonCifrar = document.getElementById('cifrar');
+
+botonCifrar.addEventListener('click', Cifrar);
+
+function Cifrar() { 
+  var texto = document.getElementById('mensaje').value;
+  var clave = parseInt(document.getElementById('llave').value);
+  var cifrado= document.getElementById('msncifrado');
+
+  var txt = "";
+  for (i=0; i <= texto.length; i++){
+     ascii=(texto.charCodeAt(i));
+     asciir= ascii+ clave
+     txt = txt + String.fromCharCode(asciir);
+     console.log(String.fromCharCode(asciir));
+  }
+  console.log(txt);
+  cifrado.value = txt;
+
+
+
+}
