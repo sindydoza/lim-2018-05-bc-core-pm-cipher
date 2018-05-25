411e8b477f5808a0f896503fed44dca57e739b6a
SinsyDoza <sindyds@gmail.com>
2018-05-25 15:09:29 -0500

    Subiendo funcion Descifrar

--- src/index.js
+++ src/index.js
@@ -2,22 +2,44 @@
 //var clave=parseInt(prompt("ingrese una llave"));
 
 var botonCifrar = document.getElementById('cifrar');
-
+var botonDesCifrar = document.getElementById('descifrar');
 botonCifrar.addEventListener('click', Cifrar);
+botonDesCifrar.addEventListener('click',Descifrado);
+
+
+
 
 function Cifrar() { 
   var texto = document.getElementById('mensaje').value;
   var clave = parseInt(document.getElementById('llave').value);
   var cifrado= document.getElementById('msncifrado');
+ 
 
-  var txt = "";
+  var txt = '';
   for (i=0; i <= texto.length; i++){
      ascii=(texto.charCodeAt(i));
      asciir= ascii+ clave
      txt = txt + String.fromCharCode(asciir);
-     console.log(String.fromCharCode(asciir));
+  
   }
   console.log(txt);
   cifrado.value = txt;
 
+}
+
+
+function Descifrado () {
+  var textoC= document.getElementById('msncifrado').value;
+  var claveD = parseInt(document.getElementById('llave').value);
+  var Descifrado = document.getElementById('msnDescifrado');
+  var txtD = '';
+
+for (d=0; d <= textoC.length; d++){
+     ascii=(textoC.charCodeAt(d));
+     asciir= ascii - claveD
+     txtD = txtD + String.fromCharCode(asciir);
+  
+  }
+
+Descifrado.value=txtD
 }
