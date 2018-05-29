100e19f41445de381e8072818d321bb6f2a8bf6d
SinsyDoza <sindyds@gmail.com>
2018-05-29 18:38:44 -0500

    Subiendo Window.cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,37 @@
+
+
 window.cipher = {
-  // ... 
+  encode:(offset, string) => {
+    var txt = '';
+    var asciir = '' ;
+    var ascii=''
+    for (let i=0; i <= string.length; i++){
+       ascii=(string.charCodeAt(i));
+       asciir= ascii+ offset
+       txt = txt + String.fromCharCode(asciir);
+    
+    }
+  
+    return txt;
+  
+  },
+  
+  
+  decode:(offset, string) => {
+
+    var txtD = '';
+    var ascii='';
+    var ascii = '';
+
+    for (let d=0; d <= string.length; d++){
+         ascii=(string.charCodeAt(d));
+         asciir= ascii - offset
+         txtD = txtD + String.fromCharCode(asciir);
+      
+      }
+    
+    return txtD;
+  }
+  
+ 
 };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 //var texto= prompt("ingrese un texo");
 //var clave=parseInt(prompt("ingrese una llave"));
 
-var botonCifrar = document.getElementById('cifrar');
-var botonDesCifrar = document.getElementById('descifrar');
-botonCifrar.addEventListener('click', Cifrar);
-botonDesCifrar.addEventListener('click',Descifrado);
+ document.getElementById('cifrar').addEventListener('click',()=>{
+    var texto = document.getElementById('mensaje').value;
+    var clave = parseInt(document.getElementById('llave').value);
+    var cifrado= document.getElementById('msncifrado');
+    cifrado.value = cipher.encode(clave, texto);
+  
+ });
 
 
 
 
-function Cifrar() { 
-  var texto = document.getElementById('mensaje').value;
-  var clave = parseInt(document.getElementById('llave').value);
-  var cifrado= document.getElementById('msncifrado');
+
+
+
+
+
+
+
+
+
+
+ document.getElementById('descifrar').addEventListener('click',() =>{
+  var textoC= document.getElementById('msncifrado').value;
+  var claveD = parseInt(document.getElementById('llave').value);
+  var Descifrado = document.getElementById('msnDescifrado');
+  Descifrado.value = cipher.decode(claveD, textoC);
+
+ }
+);
+
+ 
+
+/*function Cifrar() { 
+ 
  
 
   var txt = '';
@@ -26,20 +48,5 @@ function Cifrar() {
   cifrado.value = txt;
 
 }
+*/
 
-
-function Descifrado () {
-  var textoC= document.getElementById('msncifrado').value;
-  var claveD = parseInt(document.getElementById('llave').value);
-  var Descifrado = document.getElementById('msnDescifrado');
-  var txtD = '';
-
-for (d=0; d <= textoC.length; d++){
-     ascii=(textoC.charCodeAt(d));
-     asciir= ascii - claveD
-     txtD = txtD + String.fromCharCode(asciir);
-  
-  }
-
-Descifrado.value=txtD
-}
